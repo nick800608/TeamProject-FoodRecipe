62d0c4567521128c51f172611928f4f78dc82d4b
j40pl7llyccl <j40pl7lly@gmail.com>
2017-02-19 18:23:38 +0800

    Add files via upload

---
+++ 美食DIY(接近完成版).py
@@ -0,0 +1,89 @@
+
+# coding: utf-8
+
+# In[27]:
+
+# from collections import OrderedDict as od
+# import json
+# import csv
+# import traceback as tb            
+# import requests
+# import re #import 正規表示法相關模組
+# from bs4 import BeautifulSoup
+# res = requests.get("http://www.ttv.com.tw/cuisine/Search.aspx?lnk=tag&val=hotrecipe&page=1")
+# res.encoding = 'utf-8'
+# soup = BeautifulSoup(res.text.encode('utf-8'))
+# pagenum = int(soup.select('#ctl00_cph1_pagingResult_pnlPaging')[0].select('#ctl00_cph1_pagingResult_lblPageCount')[0].text.encode('utf-8').strip())
+# print (pagenum)#算總頁數 為了方便計算 change==> int ,觀察頁面得知每頁有15道菜
+# total_pagenum=pagenum*15
+# print(total_pagenum)#得知食譜總共有2115道菜
+
+import requests
+from bs4 import BeautifulSoup
+import traceback
+import re
+for i in range(1,142):#第37時在reup = soup.select(" #fcontent ")[0] 有error message
+    url = 'http://www.ttv.com.tw/cuisine/Detail2.aspx?rid={}'.format(i)
+#     url = 'http://www.ttv.com.tw/cuisine/Detail2.aspx?rid=1980'
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'lxml')
+    
+#     print(soup)
+    
+    mydict={} #食譜名稱
+#     title = soup.select(" .videoblock > .c490bgline > .c490con > .style15.fl.wrap")
+    for tag1 in soup.select(" .videoblock > .c490bgline > .c490con > .style15.fl.wrap"):
+        title=tag1.text
+#     print(title)
+    mydict["title"]=title
+    
+    reup = soup.select(" #fcontent ")[0]#食材和步驟中間都是 id=fcontent,下面的標籤就只有 class x了先給他一個變數,這樣好分辨
+    #爬的量一大時會有Error Message:list index out of range 在 reup = soup.select(" #fcontent ")[0]
+#     print(reup)
+    
+
+    
+    foodName = reup.select(" .style11 ")[0]
+# for tag2 in soup.select(" #fcontent > .style11 ")[0]:
+# foodName=tag2.text.strip()
+    foodName = foodName.text
+#     print(foodName)
+    mydict["foodName"]=foodName
+
+    
+#   人氣
+
+    rreup = soup.select(" #faction")[0]
+    foodPeople = rreup.select(" .style6")[0]
+    foodPeople = foodPeople.text
+#     print(foodPeople)
+    mydict["foodPeople"]=foodPeople
+
+#食材步驟
+    foodStep = reup.select(" .style11 ")[1]
+# for tag3 in soup.select(" #fcontent > .style11 ")[1]:
+#     foodStep = tag3.text.strip()
+    foodStep = foodStep.text
+#     print(foodStep)
+    mydict["foodStep"]=foodStep
+    print(mydict)
+
+
+# In[21]:
+
+import requests
+from bs4 import BeautifulSoup
+import traceback
+import re
+url = 'http://www.ttv.com.tw/cuisine/Detail2.aspx?rid=1980'
+res = requests.get(url)
+soup = BeautifulSoup(res.text, 'lxml')
+reup = soup.select(" #fcontent ")[0]
+print(reup)
+
+
+
+# In[ ]:
+
+
+
